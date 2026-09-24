c8cc78579e3a3b1800258a5c62655d85438da901
Aman Yadav <YOUR_GITHUB_EMAIL>
2026-09-24 10:26:09 +0530

    Create student feedback webpage

---
+++ store.js
@@ -0,0 +1,17 @@
+document.getElementById("feedbackForm").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let course = document.getElementById("course").value;
+    let feedback = document.getElementById("feedback").value;
+
+    document.getElementById("result").innerHTML = `
+        <h2>Submitted Feedback</h2>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Course:</strong> ${course}</p>
+        <p><strong>Feedback:</strong> ${feedback}</p>
+    `;
+
+    document.getElementById("feedbackForm").reset();
+});
